11dee839955f0a7d0c7057553c2ec86eb72bd702
4KLeeeeeee <349704698@qq.com>
2023-03-26 12:14:33 +0800

    111

--- Assets/Scripts/Mono/PlayerController.cs
+++ Assets/Scripts/Mono/PlayerController.cs
@@ -1,33 +1,182 @@
+using Spine;
+using Spine.Unity;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
 
 public class PlayerController : Controller
 {
+    SkeletonAnimation skeletonAnimation;
+    Rigidbody rb;
+    string nowState;
+    bool _stateStart;
+    TrackEntry nowEntry;
+
+    public override void AddState(string stateName)
+    {
+        base.AddState(stateName);
+        _stateStart = true;
+        if (SkeletonAnimationData.Data.ContainsKey("Player_" + stateName))
+            nowEntry = skeletonAnimation.AnimationState.SetAnimation(SkeletonAnimationData.Data["Player_" + stateName]);
+        switch (stateName)
+        {
+            default:
+                break;
+        }
+    }
+
+    public void ChangeState(string stateName)
+    {
+        if (!string.IsNullOrEmpty(nowState))
+            RemoveState(nowState);
+        AddState(stateName);
+        nowState = stateName;
+    }
     protected override void Start()
     {
         base.Start();
-        AddState("IdleState");
+        skeletonAnimation = GetComponent<SkeletonAnimation>();
+        rb = GetComponent<Rigidbody>();
+        ChangeState("IdleState");
     }
     protected override void Update()
     {
         base.Update();
     }
+
     //´ý»ú×´Ì¬
     void IdleState()
     {
-        Debug.Log("´ý»ú×´Ì¬");
-        //TODO
+        if (InputController.DirectionAxis != Vector2.zero)
+        {
+            ChangeState("WalkState");
+        }
+        if (InputController.Space > 0)
+        {
+            ChangeState("JumpState");
+        }
+        if (InputController.Fire1 > 0)
+        {
+            ChangeState("Attack");
+        }
+        if (InputController.Fire2 > 0)
+        {
+            ChangeState("Skill");
+        }
     }
     //ÒÆ¶¯×´Ì¬
-    void MoveState()
+    void WalkState()
     {
-        //TODO
+        rb.velocity = (InputController.DirectionAxis * Config.PlayerConfig.WalkSpeed).SetYToZ();
+        if (InputController.DirectionAxis.x > 0)
+        {
+            transform.rotation = new Quaternion(0, 0, 0, 1);
+        }
+        if (InputController.DirectionAxis.x < 0)
+        {
+            transform.rotation = new Quaternion(0, 1, 0, 0);
+        }
+        if (InputController.DirectionAxis == Vector2.zero)
+        {
+            ChangeState("IdleState");
+        }
+        if (InputController.Fire3 > 0)
+        {
+            ChangeState("RunState");
+        }
+        if (InputController.Space > 0)
+        {
+            ChangeState("JumpState");
+        }
+        if (InputController.Fire1 > 0)
+        {
+            ChangeState("Attack");
+        }
+        if (InputController.Fire2 > 0)
+        {
+            ChangeState("Skill");
+        }
     }
-    //ÒÆ¶¯×´Ì¬
+    //ÅÜ²½×´Ì¬
+    void RunState()
+    {
+        rb.velocity = (InputController.DirectionAxis * Config.PlayerConfig.RunSpeed).SetYToZ();
+        if (InputController.DirectionAxis.x > 0)
+        {
+            transform.rotation = new Quaternion(0, 0, 0, 1);
+        }
+        if (InputController.DirectionAxis.x < 0)
+        {
+            transform.rotation = new Quaternion(0, 1, 0, 0);
+        }
+        if (InputController.DirectionAxis == Vector2.zero)
+        {
+            ChangeState("IdleState");
+        }
+        if (InputController.Fire3 == 0)
+        {
+            ChangeState("WalkState");
+        }
+        if (InputController.Space > 0)
+        {
+            ChangeState("JumpState");
+        }
+        if (InputController.Fire1 > 0)
+        {
+            ChangeState("Attack");
+        }
+        if (InputController.Fire2 > 0)
+        {
+            ChangeState("Skill");
+        }
+    }
+    //ÌøÔ¾×´Ì¬
+    void JumpState()
+    {
+        if (_stateStart)
+        {
+            rb.AddForce(new Vector3(0, Config.PlayerConfig.JumpVelocity, 0), ForceMode.VelocityChange);
+            _stateStart = false;
+        }
+        rb.velocity = (InputController.DirectionAxis * Config.PlayerConfig.RunSpeed).SetYToZ() + new Vector3(0, rb.velocity.y, 0);
+        if (InputController.DirectionAxis.x > 0)
+        {
+            transform.rotation = new Quaternion(0, 0, 0, 1);
+        }
+        if (InputController.DirectionAxis.x < 0)
+        {
+            transform.rotation = new Quaternion(0, 1, 0, 0);
+        }
+        if (InputController.Fire1 > 0)
+        {
+            ChangeState("Attack");
+        }
+    }
+    void Attack()
+    {
+        if (_stateStart)
+        {
+            if (nowEntry != null)
+                nowEntry.Complete += (t) => { ChangeState("IdleState"); };
+            _stateStart = false;
+        }
+    }
+    void Skill()
+    {
+        if (_stateStart)
+        {
+            if (nowEntry != null)
+                nowEntry.Complete += (t) => { ChangeState("IdleState"); };
+            _stateStart = false;
+        }
+    }
+    //ÉÁ±Ü×´Ì¬
     void DodgeState()
     {
-        //TODO
+
     }
     //½©Ö±×´Ì¬
     void RigidityState()
@@ -58,5 +207,13 @@ public class PlayerController : Controller
     void SkillReleaseState()
     {
         //TODO
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Plane" && nowState == "JumpState" && rb.velocity.y <= 0)
+        {
+            ChangeState("IdleState");
+        }
     }
 }
---
+++ Assets/Scripts/Mono/TaskManager.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TaskManager : MonoBehaviour
+{
+    static TaskManager _instance;
+
+    List<Task> _runingTaskPool = new List<Task>();
+    List<Task> _removeTaskPool = new List<Task>();
+    List<Task> _pauseTaskPool = new List<Task>();
+    public static Task AddTask(Action loopFun, int times, Action callBack)
+    {
+        var task = new Task(loopFun, times, callBack);
+        _instance._runingTaskPool.Add(task);
+        return task;
+    }
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+        _instance = this;
+    }
+    private void Update()
+    {
+        foreach (var item in _runingTaskPool)
+        {
+            item.times--;
+            if (item.times <= 0)
+            {
+                item.callBack();
+                _removeTaskPool.Add(item);
+            }
+            else
+            {
+                item.loopFun();
+            }
+        }
+        if (_removeTaskPool.Count > 0)
+        {
+            foreach (var item in _removeTaskPool)
+            {
+                if (_runingTaskPool.Contains(item))
+                    _runingTaskPool.Remove(item);
+                if (_pauseTaskPool.Contains(item))
+                    _runingTaskPool.Remove(item);
+            }
+            _removeTaskPool.Clear();
+        }
+    }
+    public class Task
+    {
+        internal Action loopFun;
+        internal int times;
+        internal Action callBack;
+
+        internal Task(Action loopFun, int times, Action callBack)
+        {
+            if (loopFun == null)
+                loopFun = () => { };
+            if (callBack == null)
+                callBack = () => { };
+            this.loopFun = loopFun;
+            this.times = times;
+            this.callBack = callBack;
+        }
+        public void Stop()
+        {
+            _instance._removeTaskPool.Add(this);
+        }
+
+        public void Pause()
+        {
+            _instance._pauseTaskPool.Add(this);
+        }
+    }
+
+}
+
